febb0ffa540f5871188c4ccc5c851489a274890b
Dhahak Mohamed <dhahak@srv-tp06>
2024-10-06 18:43:25 +0200

    Sprint 1 is almost done

--- CHATBOT/src/main/java/fr/insa/maven/demo/demoMavenProject/User.java
+++ CHATBOT/src/main/java/fr/insa/maven/demo/demoMavenProject/User.java
@@ -1,5 +1,28 @@
 package fr.insa.maven.demo.demoMavenProject;
 
 public class User {
+    private String firstname;
+    private String lastname;
 
+    public User(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    // Getters and setters
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 }

a24be9fb98c32398860022658c535db01f64a30e
Dhahak Mohamed <dhahak@srv-tp06>
2024-10-06 21:47:22 +0200

    fixing errors

--- CHATBOT/src/main/java/fr/insa/maven/demo/demoMavenProject/Benevole.java
+++
@@ -1,7 +0,0 @@
-package fr.insa.maven.demo.demoMavenProject;
-
-public class Benevole extends User {
-    public Benevole(String firstname, String lastname) {
-        super(firstname, lastname);
-    }
-}
